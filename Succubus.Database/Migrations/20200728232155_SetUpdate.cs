b38be6989650742376b40be490aac858c571c327
Florian Theron <florian.theron@epitech.eu>
2020-07-29 01:22:14 +0200

    New Migration

---
+++ Succubus.Database/Migrations/20200728232155_SetUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Succubus.Database.Migrations
+{
+    public partial class SetUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Aliases",
+                table: "Sets",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Aliases",
+                table: "Sets");
+        }
+    }
+}

e55112ee9f8d6300b43547fa18cedf4476fc4f7e
Florian Theron <florian.theron@epitech.eu>
2020-07-29 01:35:36 +0200

    Update ImageRepository.cs

--- Succubus.Database/Repositories/ImageRepository.cs
+++ Succubus.Database/Repositories/ImageRepository.cs
@@ -56,7 +56,7 @@ namespace Succubus.Database.Repositories
                     .Include(x => x.Sets)
                     .AsEnumerable()
                     .Select(x => (x, Distance: x.Name.ToLowerInvariant().LevenshteinDistance(name)))
-                    .Where(x => x.Distance < 3 || x.x.Aliases.Split(',').Any(y => y.ToLowerInvariant().LevenshteinDistance(name) < 3) || x.x.Aliases.ToLowerInvariant().LevenshteinDistance(name) < 3)
+                    .Where(x => x.Distance < 3 || x.x.Aliases.Split(',').ToList().Any(x => x.ToLowerInvariant().LevenshteinDistance(name) < 3))
                     .FirstOrDefault().x;
 
                 if (cosplayer == null)
@@ -98,7 +98,7 @@ namespace Succubus.Database.Repositories
                    .AsEnumerable()
                    .Select(y => (y, Distance: y.Name.ToLowerInvariant().LevenshteinDistance(setName)))
                    .OrderBy(y => y.Distance)
-                   .Where(y => y.Distance < 3 || y.y.Aliases.Split(',').Any(x => x.ToLowerInvariant().LevenshteinDistance(setName) < 3) || y.y.Aliases.ToLowerInvariant().LevenshteinDistance(setName) < 3)
+                   .Where(y => y.Distance < 3 || y.y.Aliases.Split(',').ToList().Any(x => x.ToLowerInvariant().LevenshteinDistance(setName) < 3))
                    .FirstOrDefault().y;
 
                 if (set == null)

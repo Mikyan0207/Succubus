a84622dddd5ad90174c42e15997edb9c1fdded6b
Florian Theron <florian.theron@epitech.eu>
2021-05-11 18:43:09 +0200

    Add Projects

---
+++ Succubus.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Succubus.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}

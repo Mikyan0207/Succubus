ba94b8b9f9cfb7b2230d12d224becae697766df0
Florian Theron <florian.theron@epitech.eu>
2021-05-12 17:41:10 +0200

    ?????

--- Succubus.Web/Program.cs
+++ Succubus.Web/Program.cs
@@ -14,19 +14,12 @@ namespace Succubus.Web
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true)
-                .Build();
-
             return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder
                         .UseStartup<Startup>()
-                        .UseKestrel()
-                        .UseConfiguration(builder)
-                        .UseContentRoot(Directory.GetCurrentDirectory());
+                        .UseKestrel();
                 });
         }
     }
